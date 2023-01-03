a23774e9b96f906fa53104eaaa2673c24dff5bec
Kalia Lor <kalialorSDE@gmail.com>
2023-01-03 12:45:48 -0500

    add files

---
+++ vowel-remover.js
@@ -0,0 +1,14 @@
+//Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+//Examples
+//"hello"     -->  "hll"
+//"codewars"  -->  "cdwrs"
+//"goodbye"   -->  "gdby"
+//"HELLO"     -->  "HELLO"
+
+
+//My solution
+
+function shortcut(string){
+    return string.replace(/[aeiou]/g,'')
+  }
