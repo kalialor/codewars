b79a7881a763b07835ca62cee7c22430fc69bf43
Kalia Lor <kalialorSDE@gmail.com>
2023-01-08 10:12:25 -0500

    Logic Drills: Traffic Light

--- removestringspaces.js
+++ removestringspaces.js
@@ -1,13 +1,22 @@
-/*DIRECTIONS
+//DIRECTIONS
 
-Simple, remove the spaces from the string, then return the resultant string.
+//Simple, remove the spaces from the string, then return the resultant string.
 
 
 
-MY SOLUTION:
+//MY SOLUTION:
 
-*/
+//  \s is the regex for "whitespace"
+//  g is the "global" flag, it matches all whitespaces, it looks through the entire string
+//  what goes inside / / is the pattern
 
-function noSpace(x){
+
+
+function removeSpace(x){
   return x.replace(/\s/g, '');
 }
+
+
+
+let removeSpace = (x) => x.replace(/\s/g,'')
+
