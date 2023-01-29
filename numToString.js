c7df97a07d7b59d83e3848ac4e60a8f6c9012219
Kalia Lor <kalialorSDE@gmail.com>
2023-01-29 17:46:33 -0500

    numToString

---
+++ numToString.js
@@ -0,0 +1,20 @@
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+*/
+
+
+function numberToString(num){
+    num.ToString()
+}
+console.log(numberToString(123))
+
+
+let numberToString = (num) => num.ToString()
