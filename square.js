5d528d4701df62b86bfdb0a49a500ad44d4f3612
Kalia Lor <kalialorSDE@gmail.com>
2023-01-30 18:45:19 -0500

    square

---
+++ square.js
@@ -0,0 +1,13 @@
+/* 
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+
+//MY SOLUTION:
+
+function square(num){
+    return num * num;
+}
+
+let square = (num) => num * num 
