a6a809576b07582d7116636a1e3a6fe5190b15e7
Kalia Lor <kalialorSDE@gmail.com>
2023-02-03 02:04:22 -0500

    peronalizedMessage.js

---
+++ personalizedMessage.js
@@ -0,0 +1,23 @@
+/*
+Create a function that gives a personalized greeting. This function takes two 
+parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	                            return
+name equals owner	                'Hello boss'
+otherwise	                        'Hello guest'
+
+*/
+
+
+
+//MY SOLUTION:
+
+function greet (name,owner){
+    if(name === owner){
+        return "Hello boss" 
+    }else{
+        return "Hello guest"
+    }
+}
