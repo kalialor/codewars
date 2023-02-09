a17d233c34725c6b77a3e699cefabeddd18db546
Kalia Lor <kalialorSDE@gmail.com>
2023-02-08 21:31:35 -0500

    boolean

---
+++ booleanString.js
@@ -0,0 +1,14 @@
+/*
+Implement a function which convert the given boolean value into its 
+string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+
+//MY SOLUTION:
+
+function booleanToString(b){
+    return b.toString();
+  }
